f41e7431840cc74e20a8bce1020d1516cc3b97c1
blugsam <mijiyarow.leonid@gmail.com>
2025-08-20 14:37:17 +0300

    refactor towards MVP WIP

--- src/RescueSystem.Domain/Interfaces/IHealthProfileThresholdsRepository.cs
+++
@@ -1,9 +0,0 @@
-using RescueSystem.Domain.Entities.Health;
-using System.Linq.Expressions;
-
-namespace RescueSystem.Domain.Interfaces;
-
-public interface IHealthProfileThresholdsRepository
-{
-    Task<HealthProfileThresholds?> FindAsync(Expression<Func<HealthProfileThresholds, bool>> predicate);
-}
