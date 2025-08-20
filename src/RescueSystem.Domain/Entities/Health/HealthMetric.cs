87a23a3381d826bb8c739c7c9806cc8c6222d782
blugsam <mijiyarow.leonid@gmail.com>
2025-08-20 22:43:15 +0300

    HealthMetric class WIP

---
+++ src/RescueSystem.Domain/Entities/Health/HealthMetric.cs
@@ -0,0 +1,30 @@
+﻿namespace RescueSystem.Domain.Entities.Health;
+
+public class HealthMetric
+{
+    public Guid Id { get; private set; }
+    public Guid BraceletId { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+
+    public int? HeartRate { get; private set; }
+    public double? BodyTemperature { get; private set; }
+
+    private HealthMetric() 
+    {
+
+    }
+
+    private HealthMetric(Guid id, Guid braceletId, int? heartRate, double? bodyTemperature)
+    {
+        Id = id;
+        BraceletId = braceletId;
+        CreatedAt = DateTime.UtcNow;
+        HeartRate = heartRate;
+        BodyTemperature = bodyTemperature;
+    }
+
+    public static HealthMetric Create(Guid braceletId, int? heartRate, double? bodyTemperature)
+    {
+        return new HealthMetric(Guid.NewGuid(), braceletId, heartRate, bodyTemperature);
+    }
+}
